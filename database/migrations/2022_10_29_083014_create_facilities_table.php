00287ef82670b18c6ac50650b417e3501289900d
Muntasir Rahman <shakil_muntasir@hotmail.com>
2022-10-29 14:39:03 +0600

    HTM-20 added model, migration, controller for facility

---
+++ database/migrations/2022_10_29_083014_create_facilities_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('facilities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->bigInteger('zip');
+            $table->string('phone');
+            $table->string('email');
+            $table->unsignedBigInteger('manager_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('facilities');
+    }
+};
