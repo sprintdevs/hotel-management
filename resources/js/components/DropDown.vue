83fcf1c430616ba8bd3af7e870b0448d6f31bfe1
Rafiul Alam <alam_rafi@outlook.com>
2022-11-07 16:26:56 +0600

    HTM-24 created custom components

---
+++ resources/js/components/DropDown.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+defineProps({
+    value: null,
+    options: {
+        type: Object,
+        required: true
+    },
+    label: {
+        type: String,
+        default: ''
+    }
+})
+
+const selectedOption = null
+</script>
+<template>
+    <label class="block text-sm font-medium text-gray-700">{{ label }}</label>
+    <div class="mt-1">
+        <select
+            v-model="selectedOption"
+            @input="
+                event => {
+                    $emit('input', event.target.value)
+                }
+            "
+            class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+        >
+            <option v-for="(option, name) in options" :value="option">{{ name }}</option>
+        </select>
+    </div>
+</template>
--- resources/js/components/facilities/Form.vue
+++ resources/js/components/facilities/Form.vue
@@ -1,68 +1,35 @@
-<script setup lang="ts"></script>
 <template>
     <form action="submit" class="space-y-8 divide-y divide-gray-200">
         <div class="pt-8">
             <div class="sm:col-span-4">
-                <label for="name" class="block text-sm font-medium text-gray-700">Name</label>
-                <div class="mt-1">
-                    <input id="name" name="name" type="text" autocomplete="name" class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" />
-                </div>
+                <TextInput v-model="event.name" label="Name" type="text" />
             </div>
 
             <div class="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
                 <div class="sm:col-span-3">
-                    <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
-                    <div class="mt-1">
-                        <input id="email" name="email" type="email" autocomplete="email" class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" />
-                    </div>
+                    <TextInput v-model="event.email" label="Email" type="text" />
                 </div>
 
                 <div class="sm:col-span-3">
-                    <label for="phone" class="block text-sm font-medium text-gray-700">Phone</label>
-                    <div class="mt-1">
-                        <input type="text" name="phone" id="phone" autocomplete="phone" class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" />
-                    </div>
+                    <TextInput v-model="event.phone" label="Phone" type="text" />
                 </div>
 
                 <div class="sm:col-span-6">
-                    <label for="street-address" class="block text-sm font-medium text-gray-700">Street</label>
-                    <div class="mt-1">
-                        <input type="text" name="street-address" id="street-address" autocomplete="street-address" class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" />
-                    </div>
+                    <TextInput v-model="event.location" label="Street" type="text" />
                 </div>
 
                 <div class="sm:col-span-2">
-                    <label for="city" class="block text-sm font-medium text-gray-700">City</label>
-                    <div class="mt-1">
-                        <select id="city" name="city" autocomplete="city" class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
-                            <option v-for="city in cities" :value="city">{{ city }}</option>
-                        </select>
-                    </div>
+                    <DropDown label="City" :options="cityOptions" v-model="selectedCity" />
                 </div>
 
-                <div class="sm:col-span-2">
-                    <label for="region" class="block text-sm font-medium text-gray-700">State</label>
-                    <div class="mt-1">
-                        <select id="city" name="city" autocomplete="city" class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
-                            <option v-for="state in states" :value="state">{{ state }}</option>
-                        </select>
-                    </div>
-                </div>
+                <div class="sm:col-span-2"><DropDown label="State" :options="stateOptions" /></div>
 
                 <div class="sm:col-span-2">
-                    <label for="postal-code" class="block text-sm font-medium text-gray-700">ZIP</label>
-                    <div class="mt-1">
-                        <input type="text" name="postal-code" id="postal-code" autocomplete="postal-code" class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" />
-                    </div>
+                    <TextInput v-model="event.zip" label="ZIP" type="text" />
                 </div>
 
                 <div class="sm:col-span-6">
-                    <label for="manager" class="block text-sm font-medium text-gray-700">Manager</label>
-                    <div class="mt-1">
-                        <select id="city" name="city" autocomplete="city" class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
-                            <option v-for="manager in managers" :value="manager">{{ manager }}</option>
-                        </select>
-                    </div>
+                    <DropDown label="Manager" :options="managerOptions" v-model="selectedManager" />
                 </div>
             </div>
         </div>
@@ -76,15 +43,47 @@
 </template>
 
 <script>
+import TextInput from '../TextInput.vue'
+import DropDown from '../DropDown.vue'
 export default {
     data() {
         return {
             active: false,
-            selected: 'Dhaka',
-            cities: ['Dhaka', 'Rajshahi', 'Chittagong', 'Rangpur', 'Barishal'],
-            states: ['Dhaka', 'Rajshahi', 'Chittagong', 'Maymensingh'],
-            managers: ['Rafi', 'Shakil', 'Shourav', 'Rahee', 'Tushar', 'Hamim']
+            managers: ['Rafi', 'Shakil', 'Shourav', 'Rahee', 'Tushar', 'Hamim'],
+            selectedState: 'Apple',
+            stateOptions: {
+                Dhaka: 'Dhaka',
+                Rajshahi: 'Rajshahi',
+                Chittagong: 'Chittagong',
+                Maymensingh: 'Maymensingh'
+            },
+            selectedCity: 'Dhaka',
+            cityOptions: {
+                Dhaka: 'Dhaka',
+                Rajshahi: 'Rajshahi',
+                Chittagong: 'Chittagong',
+                Maymensingh: 'Maymensingh'
+            },
+            selectedManager: 'R',
+            managerOptions: {
+                Rafi: 'Rafi',
+                Shakil: 'Shakil',
+                Tushar: 'Tushar'
+            },
+            event: {
+                name: '',
+                phone: '',
+                email: '',
+                location: '',
+                option: '',
+                street: '',
+                city: '',
+                state: '',
+                manager: '',
+                zip: ''
+            }
         }
-    }
+    },
+    components: { TextInput, DropDown }
 }
 </script>
