7d9e0db2881447bbc156b879d01b14b66a55f118
Fardin Ahsan Sourav <fardin.ahsan10@gmail.com>
2022-11-08 16:28:04 +0600

    HTM-33 added register page

--- resources/js/router/index.ts
+++ resources/js/router/index.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import About from '../pages/About.vue'
 import Home from '../pages/Home.vue'
+import Signup from '../pages/Signup.vue'
 
 const routes: RouteRecordRaw[] = [
     { path: '/', component: Home },
-    { path: '/about', component: About }
+    { path: '/about', component: About },
+    { path: '/signup', component: Signup }
 ]
 
 const router = createRouter({
