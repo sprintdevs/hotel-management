e680eb4cb3b469c7e8e32125fdea67976b7776a0
Fardin Ahsan Sourav <fardin.ahsan10@gmail.com>
2022-12-07 17:30:44 +0600

    HTM-64 added facility header section

---
+++ resources/js/components/facilities/AddFacility.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="flex flex-row justify-between bg-gray-100 p-6">
+        <div class="flex flex-col">
+            <h2 class="font-semibold text-lg">Facility</h2>
+            <p class="text-gray-600 mt-1">A list of all the facilities including their name, title, email and role</p>
+        </div>
+        <div>
+            <RouterLink to="facilities/create">
+                <button class="rounded-md border border-transparent bg-cyan-600 py-2 px-4 text-base font-medium text-white shadow-sm hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-cyan-500 focus:ring-offset-2">Add Facility</button>
+            </RouterLink>
+        </div>
+    </div>
+</template>
--- resources/js/pages/Facilities.vue
+++ resources/js/pages/Facilities.vue
@@ -1,10 +1,7 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import AddFacility from '../components/facilities/AddFacility.vue'
+</script>
 
 <template>
-    <div class="text-center p-6">
-        This is facilities page
-        <RouterLink to="facilities/create">
-            <button class="bg-cyan-400 px-2 py-1 rounded-md">Create Facility</button>
-        </RouterLink>
-    </div>
+    <AddFacility />
 </template>
