39481ff44e094c722eca38f41e96b78dbbc62cfe
Fardin Ahsan Sourav <fardin.ahsan10@gmail.com>
2022-12-09 16:51:40 +0600

    DRAFT HTM-38 delete modal

---
+++ resources/js/components/modal/Delete.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const isOpen = ref(false)
+</script>
+
+<template>
+    <div>
+        <button @click="isOpen = true">Delete</button>
+        <div class="modal" v-if="isOpen">
+            <div>
+                <h2>Do you want to delete this item?</h2>
+                <button @click="isOpen = false">Yes</button>
+                <button @click="isOpen = false">Cancel</button>
+            </div>
+        </div>
+    </div>
+</template>
